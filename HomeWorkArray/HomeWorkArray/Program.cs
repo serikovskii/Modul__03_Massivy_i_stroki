bb8bd9b90c8b3829a683a26835e1346ea0a2a1fc
Azat <46015824+serikovskii@users.noreply.github.com>
2019-02-04 13:11:00 +0600

    dz

---
+++ HomeWorkArray/HomeWorkArray/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWorkArray
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            // 1
+            ConsoleKeyInfo Symbol;
+            int spaceCount = 0;
+            Console.WriteLine("Введите текст и точку чтобы выйти");
+            do
+            {
+                Symbol = Console.ReadKey();
+
+                if (Symbol.KeyChar == '.')
+                {
+                    break;
+                }
+                if (Symbol.KeyChar == ' ')
+                {
+                    spaceCount++;
+                }
+            }
+            while (true);
+            Console.WriteLine("\nПробелов: " + spaceCount);
+            // 2
+            int numBilet;
+            numBilet = Int32.Parse(Console.ReadLine());
+            int sumOne = 0;
+            int sumTwo = 0;
+
+            
+
+            // 3
+            string str;
+            string tmp;
+            Console.WriteLine("Введите текст:");
+            str = Console.ReadLine();
+            tmp = str;
+            if (str.Equals(tmp.ToLower()))
+            {
+                Console.WriteLine(str.ToUpper());
+            }
+            else
+            {
+                Console.WriteLine(str.ToLower());
+            }
+            Console.ReadLine();
+            // 4
+
+            // 5
+            string number = "987654321";
+            char[] numOne = number.ToCharArray();
+
+            for (int i = 0; i < numOne.Length; i++)
+            {
+                Console.Write(numOne[numOne.Length - 1 - i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
